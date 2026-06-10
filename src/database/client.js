8affc71d93993a3ca1616ac0a5df5fc24cf02ef0
Izumiko <im@cst.moe>
2026-06-10 14:23:13 +0800

    refactor: move database bootstrap modules

--- src/database/client.js
+++ src/database/client.js
@@ -7,7 +7,7 @@ const databaseExist = fs.existsSync(path.join(config.databaseFolderDir, 'db.sqli
 
 // knex 操作数据库
 const connEnv = process.env.KNEX_ENV || process.env.NODE_ENV || 'development';
-const conn = require('../../database/knexfile')[connEnv];
+const conn = require('./knexfile')[connEnv];
 const knex = require('knex')(conn);
 
 module.exports = {
